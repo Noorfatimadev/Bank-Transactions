60be953b87b7f555f5df2758e83b409df900b504
NoorFatimadev <2019cs18@student.uet.edu.pk>
2022-08-01 11:43:10 +0500

    adding project

---
+++ BankTransactions/BankTransactions/Models/Transaction.cs
@@ -0,0 +1,41 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BankTransactions.Models
+{
+    public class Transaction
+    {
+        [Key]
+        public int TransactionId { get; set; }
+
+        [Column(TypeName = "nvarchar(12)")]
+        [DisplayName("Account Number")]
+        [Required(ErrorMessage ="This Field is Required.")]
+        //[MaxLength(12, ErrorMessage = "Maximum 12 characters only.")]
+        public string  AccountName{ get; set; }
+
+        [Column(TypeName = "nvarchar(100)")] //maximum element characters
+        [DisplayName("Beneficiary Name")]
+        [Required(ErrorMessage = "This Field is Required.")]
+        public string BeneficiaryName { get; set; }
+
+        [Column(TypeName = "nvarchar(100)")]
+        [DisplayName("Bank Name")]
+        [Required(ErrorMessage = "This Field is Required.")]
+        public string BankName { get; set; }
+
+        [Column(TypeName = "nvarchar(11)")]
+        [DisplayName("SWIFT Code")]
+        [Required(ErrorMessage = "This Field is Required.")]
+        //[MaxLength(11, ErrorMessage = "Maximum 11 characters only.")]
+        public int SWIFTCode { get; set; }
+
+        [Required(ErrorMessage = "This Field is Required.")]
+        public int Amount { get; set; }
+
+        [DisplayFormat(DataFormatString ="{0:MMM-dd-yy}")]
+        public DateTime Date { get; set; }
+
+    }
+}
---
+++ BankTransactions/BankTransactions/Models/TransactionDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BankTransactions.Models
+{
+    public class TransactionDbContext:DbContext
+    {
+        //constructor
+        public TransactionDbContext(DbContextOptions<TransactionDbContext> options) : base(options) // pass same parameter tpo base
+        {
+
+        }
+
+        //physical db
+        public DbSet<Transaction> Transactions { get; set; }
+    }
+}
